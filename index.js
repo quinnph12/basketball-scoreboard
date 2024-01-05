f7a151a3d6bb59519d4a0ae7d54ed23c254a4f5d
quinnph12 <nhuquynh1612@gmail.com>
2024-01-05 22:40:49 +0800

    Troubleshoot Javascript uploading error on Netlify

--- index.js
+++ index.js
@@ -7,29 +7,35 @@ function homeScorePlusOne() {
     homeScore += 1
     homeScoreEl.textContent = homeScore
 }
+window.homeScorePlusOne = homeScorePlusOne
 
 function homeScorePlusTwo() {
     homeScore += 2
     homeScoreEl.textContent = homeScore
 }
+window.homeScorePlusTwo = homeScorePlusTwo
 
 function homeScorePlusThree() {
     homeScore += 3
     homeScoreEl.textContent = homeScore
 }
+window.homeScorePlusThree = homeScorePlusThree   
     
-    
+
 function guestScorePlusOne() {
     guestScore += 1
     guestScoreEl.textContent = guestScore
 }    
+window.guestScorePlusOne = guestScorePlusOne
 
 function guestScorePlusTwo() {
     guestScore += 2
     guestScoreEl.textContent = guestScore
 }    
+window.guestScorePlusTwo = guestScorePlusTwo
 
 function guestScorePlusThree() {
     guestScore += 3
     guestScoreEl.textContent = guestScore
 }    
+window.guestScorePlusThree = guestScorePlusThree
